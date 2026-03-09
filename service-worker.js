a6c89d8388b0eab6ef1e9dbe426427577ec4754c
zanpones <zanponesd@gmail.com>
2026-03-08 23:29:35 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,29 @@
-self.addEventListener("install", function(event) {
-console.log("App instalado")
+const CACHE = "treino-app"
+
+const urls = [
+"login.html",
+"menu.html",
+"style.css"
+]
+
+self.addEventListener("install", e => {
+
+e.waitUntil(
+
+caches.open(CACHE)
+.then(cache => cache.addAll(urls))
+
+)
+
+})
+
+self.addEventListener("fetch", e => {
+
+e.respondWith(
+
+caches.match(e.request)
+.then(response => response || fetch(e.request))
+
+)
+
 })
